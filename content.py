f861e19fd036a365db0c48003e02b72ac2c55f78
infrastructureaxolotl <153767742+paulmayhew@users.noreply.github.com>
2024-06-19 13:37:37 -0500

    new ish

---
+++ content.py
@@ -0,0 +1,75 @@
+entries = {
+    "When": {
+        "text": "In 9672 AG Timarch Radovan Karnath weds Lady Harriett of the Bene Gesserit."
+    },
+    "Selected Excerpts": {
+        "text": [
+            "* 'Excellence in Her Name' - Being a truthful account of the ascension of House Karnath",
+            "* 'Silent Meditations' the private diaries of Lady Harriett, Bene Gesserit & Timarchess of House Karnath.",
+            "* A House Is Never Divided Loyalty, Honesty, and Faith in the Eternal Truth of Data. "
+            "A Mentat Treatise for Young Initiates by Anneliese Rhedolus, Raveness and Mentat of House Karnath"
+        ]
+    },
+    "Your name": {
+        "text": "I send you into the Sisterhood or into the sky. "
+                "Either will be the end of you. Mathilde. "
+                "What heights your name still soars to even after you're gone.",
+        "image": "images/mathilde.png"
+    },
+    "Radovan, my dear": {
+        "text": "Radovid has your eyes. The little mounds of coal warm and aged like a petrol stove of old. "
+                "He has no warmth for me. I must have fire enough for us all."
+    },
+    "Your chosen": {
+        "text": "They are the finest soldiers, pilots, and engineers in the Imperium. "
+                "The Knighthawks have taken to wearing your token, what is known as Carrying the Aviatrix, "
+                "a gesture you'd have found ridiculous. "
+                "But it's for our House - and teasing your ghost conjures you in front of me again. ",
+        "image": "images/knighthawklord.png"
+    },
+    "Let us speak plainly.": {
+        "text": "I can't trust your shade anymore on this - "
+                "we've danced in front of the truth for too long. "
+                "We need to talk about what I've done. "
+                "Staring into the flames, the wreckage of your crash; "
+                "your charred remains and the ruin it represented for your family "
+                "there were but two paths forward.",
+        "image": "images/crashed.png"
+    },
+    "I did not wish to walk on either path": {
+        "text": "One of total obliteration, the other impeded only by my reluctance "
+                "to carry on for you and those you left behind."
+    },
+    "We all hold onto the only parts of you we can find": {
+        "text": "I had no designs but to step slowly "
+                "into your life in the smallest of places. I had no designs.",
+        "image": "images/radovan_painting.png"
+    },
+    "He loves me": {
+        "text": "I did not compel Radovan. That came with time. "
+                "Eventually he overcame my misgivings, and professed unworthiness. "
+                "You'd have hated me for saying those things about myself, and you'd have been half right for it."
+    },
+    "Would you still recognize me?": {
+        "text": "Does he love me because of my chance given gifts, "
+                "or what gifts I've been taught? "
+                "I can no longer separate who I was from this thing I have become."
+    },
+    "my gift; suffering in silence": {
+        "text": "Bury this guilt, carry it now, and always. "
+                "My quiet is for our children. I will be *whatever* they need me to be.",
+        "image": "path/to/suffering_in_silence.jpg"
+    },
+    "letter dated June 4671": {
+        "text": "Harriett my love for you is deathless. "
+                "It seems to bind me with mighty cables only omnipotence can break. "
+                "And yet my love for Mathilde comes over me at times like a strong wind, "
+                "and bares me irresistibly back to those unrecoverable years. "
+                "The memory of all those blissful moments come crowding over me and I feel most deeply "
+                "grateful to love and to have been loved by such creatures as these two women. "
+                "Know it is not for lack of conviction I do not discard the ashes of that former life. "
+                "And I am grateful you have never asked me to. "
+                "Hope grows anew atop what once was; the spirit of loss breathes new life into me. "
+                "I am yours darling, truly and deeply.",
+    },
+}
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
 import streamlit as st
-import yaml
+from content import entries
+import time
+import random
 
 st.set_page_config(
     page_title="Memories of a Broken Bird",
@@ -8,53 +9,86 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-with open('entries.yaml', 'r') as file:
-    data = yaml.safe_load(file)
-
-entries = list(data['texts'].items())
-
 if 'index' not in st.session_state:
     st.session_state.index = 0
 
-entry = entries[st.session_state.index % len(entries)]
+entry_keys = list(entries.keys())
+entry = entries[entry_keys[st.session_state.index % len(entry_keys)]]
 
-st.sidebar.audio("orkem_sen.ogg", format="audio/ogg", start_time=0,
-                 sample_rate=None, end_time=None, autoplay=True, loop=True)
+st.sidebar.audio("orkem_sen.ogg", format="audio/ogg", autoplay=True, loop=True)
 
-if st.sidebar.button('Next'):
-    st.session_state.index += 1
-elif st.sidebar.button('Previous'):
-    st.session_state.index -= 1
+# Create columns for navigation buttons
+col1, col2 = st.sidebar.columns(2)
+with col1:
+    if st.button('Previous'):
+        st.session_state.index = (st.session_state.index - 1) % len(entry_keys)
+with col2:
+    if st.button('Next'):
+        st.session_state.index = (st.session_state.index + 1) % len(entry_keys)
 
 st.sidebar.markdown('## Navigation:')
-for i, (title, _) in enumerate(entries):
+for i, title in enumerate(entry_keys):
     if st.sidebar.button(title):
         st.session_state.index = i
 
 # Update entry
-entry = entries[st.session_state.index % len(entries)]
+entry = entries[entry_keys[st.session_state.index % len(entry_keys)]]
 
-st.markdown(f"<h1 style='text-align: center; color: white;'>{entry[0]}</h1>", unsafe_allow_html=True)
-
+st.markdown(f"## {entry_keys[st.session_state.index % len(entry_keys)]}")
 
 def stream_data(text):
+    word_count = 0
     for word in text.split():
         yield word + " "
-        time.sleep(0.15)
+        word_count += 1
+        if word_count % 5 == 0:
+            time.sleep(0.43)
+        else:
+            delay = random.uniform(0.1, 0.34)
+            time.sleep(delay)
 
+# Display entry based on its type
+if isinstance(entry['text'], list):
+    for item in entry['text']:
+        st.markdown(item)
+else:
+    container = st.empty()
+    full_text = ""
+    for word in stream_data(entry['text']):
+        full_text += word
+        container.markdown(
+            f"""
+            <div style='text-align: center;'>
+                <h1 style='text-align: left; display: inline-block; padding-left: 30px; color: white;'>
+                    {full_text}
+                </h1>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 
-container = st.empty()
+# Add a line element
+st.markdown("----")
 
-full_text = ""
-for word in stream_data(entry[1]):
-    full_text += word
-    container.markdown(
-        f"""
-        <div style='text-align: center;'>
-            <h1 style='text-align: left; display: inline-block; padding-left: 30px; color: white;'>
-                {full_text}
-            </h1>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+# CSS for fade-in effect
+fade_in_css = """
+<style>
+.fade-in {
+    animation: fadeIn 2s;
+}
+@keyframes fadeIn {
+    from { opacity: 0; }
+    to { opacity: 1; }
+}
+</style>
+"""
+
+# Inject CSS into the Streamlit app
+st.markdown(fade_in_css, unsafe_allow_html=True)
+
+# Check if the entry has an image and display it with a fade-in effect
+if 'image' in entry:
+    image_path = entry['image']
+    st.markdown(f"<div class='fade-in' style='text-align: center;'>", unsafe_allow_html=True)
+    st.image(image_path, use_column_width=True)
+    st.markdown("</div>", unsafe_allow_html=True)
